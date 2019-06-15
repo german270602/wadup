615149369043e88b1c75fa8e5bd6d87367105612
german270602 <51804432+german270602@users.noreply.github.com>
2019-06-15 08:57:20 -0700

    Add files via upload

---
+++ clases/ClsEmpleados.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases
+{
+    public class ClsEmpleados
+    //Constructor
+    {
+    public ClsEmpleados()
+
+    {
+        Nombre = "";
+            SueldoDiario = 0.0m;
+            Edad = 0;
+        }
+
+
+
+        
+    private string _Nombre;
+
+        public decimal SueldoDiario { get; set; }
+
+        public int Edad { get; set; }
+
+
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+
+
+
+        //Metodo
+
+        public decimal CalcularSalario(int numerodias)
+    {
+        return SueldoDiario * numerodias;
+    }
+
+    }
+}
---
+++ clases/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        ClsEmpleados empleado;
+            empleado = new ClsEmpleados();
+
+            empleado.Edad = 25;
+            empleado.Nombre = "alejandro";
+            empleado.SueldoDiario = 25.6m;
+
+            decimal total;
+            total = empleado.CalcularSalario(30);
+
+            Console.WriteLine("El salario mensual del empleado" + empleado.Nombre);
+            Console.WriteLine("Es: " + total.ToString("C"));
+            Console.ReadKey();
+
+        }
+    }
+}
